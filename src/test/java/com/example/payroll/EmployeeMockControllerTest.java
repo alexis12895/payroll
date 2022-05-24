7d154029f77a675014248b9dee16092896bd7e57
Alexis Nasimba <alexis12895@gmail.com>
2022-05-24 17:29:54 +0200

    He implementado el acceso a la base de datos mariadb y tambien hago uso de Mock objets para las pruebas

---
+++ src/test/java/com/example/payroll/EmployeeMockControllerTest.java
@@ -0,0 +1,58 @@
+package com.example.payroll;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+public class EmployeeMockControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private EmployeeRepository employeeRepository;
+
+    @Test
+    void all() throws Exception {
+        List<Employee> all = new ArrayList<>();
+        all.add(new Employee("Bilbo", "Baggins","burglar"));
+        when(employeeRepository.findAll()).thenReturn(all);
+
+        mockMvc.perform(
+                        get("/employees"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void list() throws Exception {
+        List<Employee> all = new ArrayList<>();
+        all.add(new Employee("Bilbo", "Baggins","burglar"));
+        when(employeeRepository.findAll()).thenReturn(all);
+
+        mockMvc.perform(
+                        get("/employees"))
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+}
